fdda74040a9483c56a47e40443a812c7d8604303
JANG <hyoje89@naver.com>
2022-12-06 23:23:12 +0900

    coding

--- src/Pro/P20.java
+++ src/Pro/P20.java
@@ -1,5 +1,30 @@
 package Pro;
 
 public class P20 {
+    public static long solution(String numbers) {
+        long answer = 0;
+        String transNum = numbers.replace("zero","0")
+                .replace("one","1")
+                .replace("two","2")
+                .replace("three","3")
+                .replace("four","4")
+                .replace("five","5")
+                .replace("six","6")
+                .replace("seven","7")
+                .replace("eight","8")
+                .replace("nine","9");
+
+        answer = Long.parseLong(transNum);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String numbers = "onetwothreefourfivesixseveneightnine";
+
+        System.out.println(solution(numbers));
+    }
+
 
 }
