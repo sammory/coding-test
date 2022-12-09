7767359092fd17a0d769967a02322ba865d711ec
jang <hyoje89@naver.com>
2022-12-09 10:53:28 +0900

    codingtest

---
+++ src/Programmers/P25_삼각형조건.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+// 삼각형의 완성조건 12/8
+
+import java.util.Arrays;
+
+public class P25_삼각형조건 {
+    public static int solution(int[] sides) {
+        int answer;
+
+        Arrays.sort(sides);
+
+        if (sides[0] + sides[1] > sides[2]) {
+            answer = 1;
+        } else answer = 2;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] sides = {1, 2, 3};
+        System.out.println(solution(sides));
+    }
+}
