a77fb93492a14d17366f012b0467f120cf0433e0
jang <hyoje89@naver.com>
2022-12-09 13:51:05 +0900

    codingtest

--- src/Programmers/P28_A를B로만들기.java
+++ src/Programmers/P28_A를B로만들기.java
@@ -7,12 +7,12 @@ import java.util.Arrays;
 public class P28_A를B로만들기 {
     public static int solution(String before, String after) {
         int answer = 0;
-        char[] arr1 = before.toCharArray(); // 문자를 배열로 변경
+        char[] arr1 = before.toCharArray(); // 문자를 배열로 변환
         char[] arr2 = after.toCharArray();
 
         Arrays.sort(arr1); // 순서대로 정렬
         Arrays.sort(arr2);
-        String be = new String(arr1); // 문자배열을 문자열로 변경
+        String be = new String(arr1); // 문자배열을 문자열로 변환
         String af = new String(arr2);
 
         if (be.equals(af)) { // 문자 비교 equals
