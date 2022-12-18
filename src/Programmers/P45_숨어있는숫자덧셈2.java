f85ff32316567628f6b0984fc7a5ce3f654add1c
jang <hyoje89@naver.com>
2022-12-18 16:52:52 +0900

    codingtest

--- src/Programmers/P45_숨어있는숫자덧셈2.java
+++ src/Programmers/P45_숨어있는숫자덧셈2.java
@@ -3,7 +3,7 @@ package Programmers;
     문자열 my_string이 매개변수로 주어집니다.
     my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
  */
-public class P45_숨어있는숫자덧셈 {
+public class P45_숨어있는숫자덧셈2 {
     public static int solution(String my_string) {
         int answer = 0;
 
