539770556a9d72411dba39c7f30d96d8e7e836b3
jang <hyoje89@naver.com>
2023-01-10 13:00:14 +0900

    codingtest

---
+++ src/Programmers/lv1/P17_음양더하기.java
@@ -0,0 +1,31 @@
+package Programmers.lv1;
+
+public class P17_음양더하기 {
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i] ==  true) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+/*  간결한 코드
+    int answer = 0;
+    for (int i=0; i<signs.length; i++)
+        answer += absolutes[i] * (signs[i]? 1: -1);
+    return answer;
+ */
+
+
+    public static void main(String[] args) {
+
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};
+        System.out.println(solution(absolutes,signs));
+    }
+}
