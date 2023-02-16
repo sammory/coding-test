61816c2ebffd649c45a2f85cc8e425e6d465be63
jang <hyoje89@naver.com>
2023-02-16 23:20:27 +0900

    codingtest

--- src/Programmers/lv2/P02_뒤에있는큰수찾기.java
+++ src/Programmers/lv2/P02_뒤에있는큰수찾기.java
@@ -1,8 +1,11 @@
 package Programmers.lv2;
 
 import java.util.Stack;
-
-public class P02_숫자변환하기 {
+/*
+    정수로 이루어진 배열 numbers가 있습니다. 배열 의 각 원소들에 대해 자신보다 뒤에 있는 숫자 중에서 자신보다 크면서 가장 가까이 있는 수를 뒷 큰수라고 합니다.
+    정수 배열 numbers가 매개변수로 주어질 때, 모든 원소에 대한 뒷 큰수들을 차례로 담은 배열을 return 하도록 solution 함수를 완성해주세요. 단, 뒷 큰수가 존재하지 않는 원소는 -1을 담습니다.
+ */
+public class P02_뒤에있는큰수찾기 {
     public static int[] solution(int[] numbers) {
         // 인덱스를 스택으로 관리
         // 뒷수가 앞수보다 클때는 answer에 값을 넣고 스택에는 제거, 반대 경우일때는 스택에 기록되있는 인덱스로 값 처리
